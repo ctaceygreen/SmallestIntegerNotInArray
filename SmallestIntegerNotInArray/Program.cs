4df28bd4095886333aa8e923b09cc87c6b181848
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 13:14:42 +0000

    Add project files.

---
+++ SmallestIntegerNotInArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestIntegerNotInArray
+{
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            HashSet<int> numbersInA = new HashSet<int>();
+            foreach (var item in A)
+            {
+                if (item > 0)
+                {
+                    numbersInA.Add(item);
+                }
+            }
+
+            bool foundMissing = false;
+            int value = 1;
+            while(!foundMissing)
+            {
+                if (!numbersInA.Contains(value))
+                {
+                    foundMissing = true;
+                }
+                else
+                {
+                    value++;
+                }
+            }
+            return value;
+
+        }
+    }
+}
